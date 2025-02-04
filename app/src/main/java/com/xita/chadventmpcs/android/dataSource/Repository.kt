7bfdde37968caf4a95d499d32538e49092b09b0d
Tejiri <steveijatomi@gmail.com>
2025-02-04 22:23:33 +0000

    version one published to playstore

--- app/src/main/java/com/xita/chadventmpcs/android/dataSource/Repository.kt
+++ app/src/main/java/com/xita/chadventmpcs/android/dataSource/Repository.kt
@@ -8,21 +8,20 @@ import com.xita.chadventmpcs.android.models.UserLogin
 import com.xita.chadventmpcs.android.models.UserResponse
 
 class Repository {
-    val api_key = "5fd2ac39-15d3-4935-a36a-509597984923"
     val apiKey = BuildConfig.API_KEY
 
 
     suspend fun getMembers(): List<Member> {
-        return RetrofitInstance.chadventAPI.getMembers(api_key)
+        return RetrofitInstance.chadventAPI.getMembers(apiKey)
     }
 
     suspend fun getAccounts(): List<Account> {
-        return RetrofitInstance.chadventAPI.getAccounts(api_key)
+        return RetrofitInstance.chadventAPI.getAccounts(apiKey)
     }
 
     suspend fun logUserIn(userLogin: UserLogin): UserResponse {
         return RetrofitInstance.chadventAPI.logUserIn(
-            apiKey = api_key,
+            apiKey = apiKey,
             username = userLogin.username,
             password = userLogin.password
         )
